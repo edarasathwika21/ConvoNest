e60206e785d4341a96c63e2832e0ffa457da4726
Edara Sathwika <eedarasathwika@gmail.com>
2024-08-19 12:42:44 +0530

    backend

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import messageRoutes from "./routes/message.route.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser())
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
