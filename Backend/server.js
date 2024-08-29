f03ff91448f0901f9670b3490f92f87b9f13726d
Edara Sathwika <eedarasathwika@gmail.com>
2024-08-29 10:01:09 +0530

    Add functionality to connect frontend and backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -12,6 +13,8 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 
+const __dirname=path.resolve()
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser())
@@ -19,6 +22,12 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
